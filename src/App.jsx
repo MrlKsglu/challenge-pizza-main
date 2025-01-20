dfb9db1a38fdc6e7299a8baf4ac5b254d9c09cff
MrlKsglu <meralkuss@gmail.com>
2025-01-20 11:20:19 +0300

    ana sayfa düzenlendi

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import workintech from '/workintech.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <body>
+
+        <div>
+          <h1>Teknolojik Yemekler</h1>
+          <h2>KOD ACIKTIRIR</h2>
+          <h2>PİZZA, DOYURUR</h2>
+          <button class="custom-button">ACIKTIM</button>
+
+        </div>
+        
+          
+
+        
+      </body>
+    </>
+  )
+}
+
+export default App
